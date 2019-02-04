b4f268eaad5ff656dfdc49b78edc01dd2c164e9c
Artem Kalashnikov <kalashnikov.tema@gmail.com>
2019-02-04 16:51:05 +0300

    Remove babel-polyfill import

--- src/WavesLedger.ts
+++ src/WavesLedger.ts
@@ -1,7 +1,6 @@
 /// <reference path="../interface.d.ts"/>
 
 
-import 'babel-polyfill';
 import { Waves, IUserData } from './Waves';
 import { default as TransportU2F } from '@ledgerhq/hw-transport-u2f';
 
